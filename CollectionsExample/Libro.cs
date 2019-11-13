646f97e00e7b651ad9b1945755239c8771f9f6cf
axiadotnet <57347435+axiadotnet@users.noreply.github.com>
2019-11-13 14:41:28 +0100

    first commit

---
+++ CollectionsExample/Libro.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace CollectionsExample
+{
+    class Libro : IComparable<Libro>
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PageNuber { get; set; }
+
+        public Libro(string title, string author, int pageNuber)
+        {
+            Title = title;
+            Author = author;
+            PageNuber = pageNuber;
+        }
+
+        public override string ToString()
+        {
+            return Title + " " + Author + " " + PageNuber;
+        }
+
+        public int CompareTo([AllowNull] Libro other)
+        {
+            return Title.CompareTo(other.Title);
+        }
+    }
+
+    class LibroComparer : IComparer<Libro>
+    {
+
+        public LibroOrderType OrderType { get; set; }
+
+        public LibroComparer(LibroOrderType orderType)
+        {
+            OrderType = orderType;
+        }
+
+        public int Compare([AllowNull] Libro l1, [AllowNull] Libro l2)
+        {
+            switch (OrderType)
+            {
+                case LibroOrderType.Title:
+                    return l1.Title.CompareTo(l2.Title);
+                case LibroOrderType.Author:
+                    return l1.Author.CompareTo(l2.Author);
+                case LibroOrderType.Pages:
+                    return l1.PageNuber.CompareTo(l2.PageNuber);
+                default:
+                    return l1.Title.CompareTo(l2.Title);
+            }
+        }
+    }
+
+    enum LibroOrderType:int
+    {
+        Title = 0, Author = 1, Pages = 2
+    }
+}
---
+++ CollectionsExample/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> pippo = new List<int>(20);
+            pippo.Add(5);
+            pippo.Add(5);
+            pippo.Add(5);
+            pippo.Add(5);
+            pippo.Add(5);
+            pippo.Add(5);
+            pippo.Add(5);
+
+            pippo.TrimExcess();
+
+            List<int> pluto = new List<int>() {1,7,9,13,4};
+
+            List<string> paperino = new List<string>() { "uno", "cavolo", "osso"};
+
+            paperino.Add("cavallo");
+
+            paperino.Insert(1, "ufo");
+
+            //foreach (string item in paperino)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            paperino[0] = "due";
+
+            //for (int i = 0; i < paperino.Count; i++)
+            //{
+            //    Console.WriteLine(paperino[i]);
+            //}
+
+            List<Libro> libri = new List<Libro>();
+
+            libri.Add(new Libro("odissea", "omero", 200));
+
+            Libro nuovoLibro = new Libro("iliade", "omero", 150);
+
+            libri.Add(nuovoLibro);
+
+            libri.Add(new Libro("eneide", "virgilio", 230));
+
+            libri.Add(new Libro("IT", "king", 500));
+
+            libri.Add(new Libro("Oceano", "baricco", 500));
+
+            libri.Sort(new LibroComparer(LibroOrderType.Pages));
+
+            foreach (var item in libri)
+            {
+                Console.WriteLine(item);
+            }
+
+            //libri.Remove(nuovoLibro);
+
+            //libri.RemoveAt(1);
+
+            //Console.WriteLine();
+
+            //foreach (var item in libri)
+            //{
+            //    Console.WriteLine(item);
+            //}
+        }
+    }
+}
